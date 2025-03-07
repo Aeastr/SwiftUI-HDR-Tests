71a97af0c09311eb3330847da0f878c9f3721a85
Aether <64797587+Aeastr@users.noreply.github.com>
2025-03-07 15:48:34 +0000

    Initial Commit

---
+++ HDRTesting/HDRTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HDRTestingApp.swift
+//  HDRTesting
+//
+//  Created by Aether on 07/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct HDRTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
